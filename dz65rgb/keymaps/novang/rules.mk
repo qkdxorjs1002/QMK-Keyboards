d86de5152a9e0ae992e197499958c5ecd0838936
Novang <qkdxorjs1002@naver.com>
2019-10-20 19:33:19 +0900

    Add my keymap

---
+++ dz65rgb/keymaps/novang/rules.mk
@@ -0,0 +1,17 @@
+BOOTMAGIC_ENABLE = lite				# Virtual DIP switch configuration
+MOUSEKEY_ENABLE = yes				# Mouse keys
+EXTRAKEY_ENABLE = yes				# Audio control and System control
+CONSOLE_ENABLE = no					# Console for debug
+COMMAND_ENABLE = no					# Commands for debug and configuration
+NKRO_ENABLE = yes					# USB Nkey Rollover
+AUDIO_ENABLE = no					# Use Audio
+MIDI_ENABLE = no            		# MIDI controls
+UNICODE_ENABLE = no         		# Unicode
+BLUETOOTH_ENABLE = no       		# Enable Bluetooth with the Adafruit EZ-Key HID
+BACKLIGHT_ENABLE = no				# Use Backlight
+RGB_MATRIX_ENABLE = yes				# Use RGB matrix
+SLEEP_LED_ENABLE = no       		# Breathing sleep LED during USB suspend
+
+VELOCIKEY_ENABLE = yes
+
+LINK_TIME_OPTIMIZATION_ENABLE = yes
