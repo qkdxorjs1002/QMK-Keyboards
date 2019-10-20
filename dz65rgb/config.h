cfdf53b582cefdc7f754dd107724a99332bbad29
Novang <qkdxorjs1002@naver.com>
2019-10-20 19:32:29 +0900

    Increase max rgb brightness

--- dz65rgb/config.h
+++ dz65rgb/config.h
@@ -5,21 +5,21 @@
 #define DEVICE_VER      0x0001
 #define MANUFACTURER    DZTECH
 #define PRODUCT         DZ65RGB
-#define DESCRIPTION     DZ65 ARM RGB keyboard
+#define DESCRIPTION     DZ65RGB ARM RGB keyboard
 
 #define MATRIX_ROWS 5
 #define MATRIX_COLS 15
 #define MATRIX_ROW_PINS { B1, B10, B11, B14, B12 }
-#define MATRIX_COL_PINS {A6, A7, B0, B13, B15, A8, A15, B3, B4, B5, B8, B9, C13, C14, C15 }
+#define MATRIX_COL_PINS { A6, A7, B0, B13, B15, A8, A15, B3, B4, B5, B8, B9, C13, C14, C15 }
 #define DIODE_DIRECTION COL2ROW
 
-#define RGB_MATRIX_LED_PROCESS_LIMIT 4
-#define RGB_MATRIX_LED_FLUSH_LIMIT 26
+#define RGB_MATRIX_LED_PROCESS_LIMIT 4          // limits the number of LEDs to process in an animation per task run (increases keyboard responsiveness)
+#define RGB_MATRIX_LED_FLUSH_LIMIT 26           // limits in milliseconds how frequently an animation will update the LEDs. 16 (16ms) is equivalent to limiting to 60fps (increases keyboard responsiveness)
 #define DEBOUNCE 3
-#define RGB_DISABLE_AFTER_TIMEOUT 0 // number of ticks to wait until disabling effects
-#define RGB_DISABLE_WHEN_USB_SUSPENDED true // turn off effects when suspended
-#define RGB_MATRIX_KEYPRESSES
-#define RGB_MATRIX_MAXIMUM_BRIGHTNESS 180
+#define RGB_DISABLE_AFTER_TIMEOUT 0             // number of ticks to wait until disabling effects
+#define RGB_DISABLE_WHEN_USB_SUSPENDED true     // turn off effects when suspended
+#define RGB_MATRIX_KEYPRESSES                   // reacts to keypresses
+#define RGB_MATRIX_MAXIMUM_BRIGHTNESS 200       // limits maximum brightness of LEDs to 200 out of 255. If not defined maximum brightness is set to 255
 
 #define DRIVER_ADDR_1 0b1110100
 #define DRIVER_ADDR_2 0b1110111
