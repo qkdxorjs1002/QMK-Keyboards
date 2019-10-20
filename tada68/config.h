5baf554ae978139dfe5aead5d771a37285b8e163
Novang <qkdxorjs1002@naver.com>
2019-10-20 19:40:10 +0900

    Update config.h

--- tada68/config.h
+++ tada68/config.h
@@ -26,7 +26,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define DEVICE_VER      0x0003
 #define MANUFACTURER    TADA
 #define PRODUCT         TADA68
-#define DESCRIPTION     QMK keyboard firmware for TADA68 with WS2812 support
+#define DESCRIPTION     "QMK keyboard firmware for TADA68 with WS2812 support"
 
 /* key matrix size */
 #define MATRIX_ROWS 5
